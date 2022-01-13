ce9a039bd9f1bfc8bf7d15c560bdb79cf5b9df59
Sagar <sagarpatel1551@icloud.com>
2022-01-12 23:34:36 -0500

    Added more content

---
+++ javascript/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.center').slick({
+        centerMode: true,
+        slidesToShow: 2,
+        autoplay: true,
+        autoplaySpeed: 4000,
+    });
+  });           
